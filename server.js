938b31e08ae857c4339e0236a49e304041550dcb
Charles Loehle <clguitar1@gmail.com>
2020-03-30 14:35:55 -0400

    prepared for netlify deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // custom path to config variables
 dotenv.config({ path: './config/config.env' });
@@ -20,6 +21,14 @@ if (process.env.NODE_ENV === 'development') {
 connectDB();
 
 app.use('/api/v1/transactions', require('./routes/transactions-route'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
